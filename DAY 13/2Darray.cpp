dc97a90bf318f98878b8fdd9cc459e19847ea2a3
ayush01259 <ayushkumar01259@gmail.com>
2026-06-07 18:22:45 +0530

    new

--- DAY 13/2Darray.cpp
+++ DAY 13/2Darray.cpp
@@ -4,16 +4,16 @@ int main(){
     int arr[3][4];
     int n = 3, m = 4;
     for(int i =0; i<n; i++){
-        for(int j= 0; j<n; j++){
+        for(int j= 0; j<m; j++){
         cin>>arr[i][j];
         }
     }
 
     for(int i =0; i<n; i++){    
-        for(int j=0; j<n; j++){
-        cout<<arr[i][j];
+        for(int j=0; j<m; j++){
+        cout<<arr[i][j]<<" ";
         }
-    cout<<endl;
+        cout<<endl;
     }
 
 
