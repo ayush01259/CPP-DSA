6d96b2b067c42e9ede65b901cb08a4ccf2efcaa3
ayush01259 <ayushkumar01259@gmail.com>
2026-06-07 23:21:00 +0530

    day 13

---
+++ DAY 13/spiralmatrix.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+void spiralMatrix(int mat[][4], int n, int m){
+    int srow = 0, scol = 0;
+    int erow = n-1, ecol = m-1;
+
+    while(srow<=erow && scol<=ecol){
+
+        // top row of a matrix
+        for ( int j = scol; j<=ecol; j++){
+            cout<<mat[srow][j] << " ";
+        }
+
+        // right  col of the matrix 
+        for(int i = srow+1; i<=erow; i++){
+            cout<<mat[i][ecol]<<" ";
+        }
+
+        // bottom col of the matrix
+        for(int j = ecol-1; j>=scol;j--){
+            if(srow==erow){
+                break;
+            }
+            cout<<mat[erow][j]<<" ";
+        }
+
+        for(int i =erow-1; i>=srow+1; i--){
+            if(scol==ecol){
+                //its a corner case that happens in non square matrices and help to prevent the reappearance of middle row and middle column
+                break;
+            }
+            cout<<mat[i][scol]<<" ";
+        }
+
+        srow++; scol++;
+        erow--; ecol--;
+
+    }
+    cout<<endl;
+}
+
+int main(){
+    int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+    spiralMatrix(matrix,4,4);
+
+
+    return 0;
+}
