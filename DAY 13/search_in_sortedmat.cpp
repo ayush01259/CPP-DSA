d90a1326115f7af24531108f31138acb14118ac4
ayush01259 <ayushkumar01259@gmail.com>
2026-06-09 17:20:02 +0530

    Day 13

---
+++ DAY 13/search_in_sortedmat.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool search( int mat[][4], int n , int m, int key){
+    int i = 0, j = m-1;
+    while(i < n && j >=0){
+        if(mat[i][j] == key){
+            cout<<"Key Found at cell (" << i << " , " << j << ")\n";
+            return true;
+        }else if( mat[i][j] > key){
+            j--;
+        }else{
+            i++;
+        }
+    }
+    cout<<"Key not Found \n";
+    return false;
+}
+
+int main(){
+    int matrix[4][4] = {{10,20,30,40},
+                        {15,25,35,45},
+                        {27,29,37,48},
+                        {32,33,39,50}};
+
+    search(matrix, 4,4, 48);
+    search(matrix, 4,4, 42);
+    return 0;
+}
