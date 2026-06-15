efc2bff4d8d59966c16a35dbdcecc756b37e78f2
ayush01259 <ayushkumar01259@gmail.com>
2026-06-15 09:20:25 +0530

    Day 16
    added setith bit here that work on or principle with 0 and 1 and it increases the value here

--- DAY 16/bitmasking.cpp
+++ DAY 16/bitmasking.cpp
@@ -12,9 +12,16 @@ int getithbit(int num, int i){
     }
 }
 
+int setitbit(int num, int i){
+    int bitmask = 1<<i;
+    return (num | bitmask);
+}
+
 int main(){
     cout<<getithbit(6,2)<<endl;
     cout<<getithbit(8,3)<<endl;
     cout<<getithbit(9,3)<<endl;
+    cout<<setitbit(8,1)<<endl;
+
     return 0;
 }
