285ea14ee9769e4d2ac360eec561a195fd20b8c9
ayush01259 <ayushkumar01259@gmail.com>
2026-06-16 18:27:17 +0530

    day 17

---
+++ DAY 17 (OOPS)/basics/copy.cpp
@@ -0,0 +1,31 @@
+//deep and shallow copy concepts
+
+#include<iostream>
+using namespace std;
+class Car{
+    public: 
+    string name;
+    string color;
+    int *mileage;
+    Car (string name, string color){
+        this->name = name; // static allocaiton
+        this->color = color;
+        mileage = new int; // Dynamic allocation
+        *mileage = 12;
+    }
+    Car (Car &original){
+        cout<<"Copying original to new...\n";
+        name = original.name;
+        color = original.color;
+        mileage = original.mileage;
+    }
+};
+int main(){
+    
+    Car c1("Toyota LC","Black");
+    Car c2(c1);
+    cout<<c2.name<<endl;
+    cout<<c2.color<<endl;
+    cout<<*c2.mileage<<endl;
+    return 0;
+}
